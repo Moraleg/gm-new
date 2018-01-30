9e544cce7280052732510af56b788ffff0584789
Geraldine Morales <geraldine.morales02@gmail.com>
2018-01-29 19:12:46 -0500

    testing mobile scroll

--- js/scrollJack.js
+++ js/scrollJack.js
@@ -6,15 +6,15 @@
   			  Variables
   	\*=========================================*/
 
-	let delta = 0;
-	let currentSlideIndex = 0;
-   const  scrollThreshold = 45;
-   const dragThreshold = 0.15; // "percentage" to drag before engaging
+	let delta = 0; //distance scrolled/dragged in slide
+	let currentSlideIndex = 0; // the current slide
+   const  scrollThreshold = 45; // px to scroll before engaging
+   const dragThreshold = .15; // "percentage" to drag before engaging
 	const slides = $('.slide');
    const slideNavSelector = $('.projects');
 	const navSlides = $('.nav-slide');
    const activeSlideClass = ('active');
-	const numSlides = slides.length - 1;
+	const numSlides = slides.length - 1; // total number of slides
 
 
    /*=========================================*\
@@ -71,7 +71,9 @@
 		// reset
 		delta = 0;
 
-		slides.each(function(i, slide, slideNav) {
+
+      //loop over slides and add the correct active classes
+		slides.each(function(i, slide) {
 			$(slide).toggleClass('active', (i >= currentSlideIndex));
 		});
 
@@ -79,7 +81,7 @@
       //update the navigation
       updateSlideNav(currentSlideIndex);
 
-      //return the current panel
+      //return the current slide
 		return currentSlideIndex;
 
 	}
@@ -97,7 +99,7 @@
 
 		showSlide(currentSlideIndex);
 
-      // return the current panel
+      // return the current slide
 	   return currentSlideIndex;
 	}
 
@@ -111,7 +113,7 @@
 
 		showSlide(currentSlideIndex);
 
-      //return the current panel
+      //return the current slide
       return currentSlideIndex;
 	}
 
@@ -158,6 +160,91 @@
 
          e.preventDefault();
 	});
+
+
+   // mobile
+
+    var dragStart = null;	 // used to determine touch / drag distance
+    var percentage = 0;
+    var target,
+    previousTarget;
+    var windowHeight = $(window).height();
+
+function touchStart(event) {
+
+	if (dragStart !== null) { return; }
+	if (event.originalEvent.touches) {
+		event = event.originalEvent.touches[0];
+	}
+
+	// where in the viewport was touched
+	dragStart = event.clientY;
+
+	// make sure we're dealing with a slide
+	target = slides.eq(currentSlideIndex)[0];
+
+	// disable transitions while dragging
+	target.classList.add('no-animation');
+
+	previousTarget = slides.eq(currentSlideIndex-1)[0];
+	previousTarget.classList.add('no-animation');
+}
+
+function touchMove (event) {
+
+	if (dragStart === null) { return; }
+	if (event.originalEvent.touches) {
+		event = event.originalEvent.touches[0];
+	}
+
+	delta = dragStart - event.clientY;
+	percentage = delta / windowHeight;
+
+	// Going down/next. Animate the height of the target element.
+	if (percentage > 0) {
+		target.style.height = (100-(percentage*100))+'%';
+		if (previousTarget) {
+			previousTarget.style.height = ''; 	// reset
+		}
+	}
+
+	// Going up/prev. Animate the height of the _previous_ element.
+	else if (previousTarget) {
+		previousTarget.style.height = (-percentage*100)+'%';
+		target.style.height = '';	// reset
+	}
+
+	// Don't drag element. This is important.
+	return false;
+}
+
+function touchEnd () {
+
+	dragStart = null;
+	target.classList.remove('no-animation');
+	if (previousTarget) {
+		previousTarget.classList.remove('no-animation');
+	}
+
+	if (percentage >= dragThreshold) {
+		nextSlide();
+	}
+	else if ( Math.abs(percentage) >= dragThreshold ) {
+		prevSlide();
+	} else {
+		// show current slide i.e. snap back
+		showSlide();
+	}
+
+	percentage = 0;
+
+}
+
+$('#projects').on({
+	'touchstart': touchStart,
+	'touchmove': touchMove,
+	'touchend': touchEnd
+});
 
 
 
