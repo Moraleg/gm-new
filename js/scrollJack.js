0b6771315d3735164b47c1d8f6928465b4cea341
Geraldine Morales <geraldine.morales02@gmail.com>
2018-02-18 19:47:43 -0500

    cleaned up code

--- js/scrollJack.js
+++ js/scrollJack.js
@@ -1,27 +1,27 @@
-
 // Scroll Jack
-(($) => {
 
-   /*=========================================*\
-  		Variables
-  	\*=========================================*/
+$(function(){
+
+
+	/*=========================================*\
+	Variables
+	\*=========================================*/
 
 	let delta = 0; //distance scrolled/dragged in slide
 	let currentSlideIndex = 0; // the current slide
-   const  scrollThreshold = 45; // px to scroll before engaging
-   const dragThreshold = 0.15; // "percentage" to drag before engaging
+	const  scrollThreshold = 45; // px to scroll before engaging
+	const dragThreshold = 0.15; // "percentage" to drag before engaging
 	const slides = $('.slide');
-   const slideNavSelector = $('.projects');
 	const navSlides = $('.nav-slide');
 	const numSlides = slides.length - 1; // total number of slides
-	const intro = $('.intro-container p') //change nav text per section
+	const intro = $('.intro-container p'); //change nav text per section
 
 
 
-   /*=========================================*\
-      Scroll - DOMScroll and
-      mousewheel events
-   \*=========================================*/
+	/*=========================================*\
+		Scroll - DOMScroll and
+		mousewheel events
+	\*=========================================*/
 
 	const elementScroll = (e) => {
 
@@ -36,7 +36,6 @@
 				prevSlide();
 			}
 
-
 		} else { // --- Scrolling down ---
 
 			delta++;
@@ -48,45 +47,36 @@
 
 		// Prevent page from scrolling
 		return false;
-
 	}
 
-   // On Mouse Scroll
-   $(window).on({
-      'DOMMouseScroll mousewheel': elementScroll
-   });
+	// On Mouse Scroll
+	$(window).on({'DOMMouseScroll mousewheel': elementScroll});
 
+	/*=========================================*\
+		Slide control functions
 
-   /*=========================================*\
-      Slide control functions
-
-      Used to update slide via any nav menthod
-
-         -showSlide
-         -prevSlide
-         -nextSlide
-         -updateSlideNav
-   \*=========================================*/
-
+		Used to update slide via any nav menthod
+		-showSlide
+		-prevSlide
+		-nextSlide
+		-updateSlideNav
+	\*=========================================*/
 
 	const showSlide = () => {
 
 		// reset
 		delta = 0;
 
-
-      //loop over slides and add the correct active classes
+		//loop over slides and add the correct active classes
 		slides.each(function(i, slide) {
 			$(slide).toggleClass('active', (i >= currentSlideIndex));
 		});
 
+		//update the navigation
+		updateSlideNav(currentSlideIndex);
 
-      //update the navigation
-      updateSlideNav(currentSlideIndex);
-
-      //return the current slide
+		//return the current slide
 		return currentSlideIndex;
-
 	}
 
 
@@ -100,8 +90,8 @@
 
 		showSlide(currentSlideIndex);
 
-      // return the current slide
-	   return currentSlideIndex;
+		// return the current slide
+		return currentSlideIndex;
 	}
 
 	const nextSlide = () => {
@@ -114,29 +104,28 @@
 
 		showSlide(currentSlideIndex);
 
-      //return the current slide
-      return currentSlideIndex;
+		//return the current slide
+		return currentSlideIndex;
 	}
 
-   const updateSlideNav = function(currentSlideIndex) {
-      navSlides.removeClass('active');
-      navSlides.eq(currentSlideIndex).addClass('active');
-   };
+	const updateSlideNav = function(currentSlideIndex) {
+		navSlides.removeClass('active');
+		navSlides.eq(currentSlideIndex).addClass('active');
+	};
 
 
-   /*=========================================*\
-   	Alternate slide navigation
-   		- arrow keys
-   		- nav click event
-   \*=========================================*/
+	/*=========================================*\
+		Alternate slide navigation
+		- arrow keys
+		- nav click event
+	\*=========================================*/
 
 	// On Arrow Keys
 	$(document).keyup((e) => {
-
 		// Left or back arrows
 		if ((e.which === 37) ||  (e.which === 38)){
 			prevSlide();
-         introChange();
+			introChange();
 			console.log(currentSlideIndex);
 		}
 		// Down or right
@@ -149,20 +138,20 @@
 	// On Nav Control - Nav Click event
 	$('.nav-slide').on('click', function(e) {
 
-      //Get current slide
-      currentSlideIndex = $(this).data('slide');
-      console.log(currentSlideIndex);
+		//Get current slide
+		currentSlideIndex = $(this).data('slide');
+		// console.log(currentSlideIndex);
 
-      // When link clicked, find slide it points to
-      const newslide = parseInt($(this).data('slide'));
+		// When link clicked, find slide it points to
+		const newslide = parseInt($(this).data('slide'));
 
-      // find how far it is from current slide
-      const diff = newslide - currentSlideIndex - 1;
-      showSlide(diff); // show that slide
+		// find how far it is from current slide
+		const diff = newslide - currentSlideIndex - 1;
+		showSlide(diff); // show that slide
 
 		introChange();
 
-      e.preventDefault();
+		e.preventDefault();
 	});
 
 	//intro change function
@@ -170,28 +159,27 @@
 	const introChange = () => {
 
 		if(currentSlideIndex === 1) {
-
-			for (let i = 0; i < intro.length; i++) {
+			for (i = 0; i < intro.length; i++) {
 				intro[i].innerHTML = 'PROJ<br>ECT<br>001';
 			}
 		} else if (currentSlideIndex === 2){
-			for (let i = 0; i < intro.length; i++) {
+			for (i = 0; i < intro.length; i++) {
 				intro[i].innerHTML = 'PROJ<br>ECT<br>002';
 			}
 		} else if (currentSlideIndex === 3) {
-			for (let i = 0; i < intro.length; i++) {
+			for (i = 0; i < intro.length; i++) {
 				intro[i].innerHTML = 'PROJ<br>ECT<br>003';
 			}
 		} else if (currentSlideIndex == 4) {
-			for (let i = 0; i < intro.length; i++) {
+			for (i = 0; i < intro.length; i++) {
 				intro[i].innerHTML = 'PROJ<br>ECT<br>004';
 			}
 		} else if (currentSlideIndex === 5) {
-			for (let i = 0; i < intro.length; i++) {
+			for (i = 0; i < intro.length; i++) {
 				intro[i].innerHTML = 'PROJ<br>ECT<br>005';
 			}
 		} else {
-			for (let i = 0; i < intro.length; i++) {
+			for (i = 0; i < intro.length; i++) {
 				intro[i].innerHTML = 'INT<br>RO';
 			}
 		}
@@ -199,92 +187,88 @@
 	};// end introchange
 
 
-   // mobile
+	// mobile
 
-    let dragStart = null;	 // used to determine touch / drag distance
-    let percentage = 0;
-    let target,
-    previousTarget;
+	let dragStart = null;	 // used to determine touch / drag distance
+	let percentage = 0;
+	let target,
+	previousTarget;
 
-   const touchStart = (e) => {
+	const touchStart = (e) => {
 
-   	if (dragStart !== null) { return; }
-   	if (e.originalEvent.touches) {
-   		event = e.originalEvent.touches[0];
-   	}
+		if (dragStart !== null) { return; }
+		if (e.originalEvent.touches) {
+			event = e.originalEvent.touches[0];
+		}
 
-   	// where in the viewport was touched
-   	dragStart = event.clientY;
+		// where in the viewport was touched
+		dragStart = event.clientY;
 
-   	// make sure we're dealing with a slide
-   	target = slides.eq(currentSlideIndex)[0];
-      console.log(target);
+		// make sure we're dealing with a slide
+		target = slides.eq(currentSlideIndex)[0];
 
-   	previousTarget = slides.eq(currentSlideIndex-1)[0];
+		previousTarget = slides.eq(currentSlideIndex-1)[0];
+
 		introChange();
-   };
+	};
 
-   const touchMove = (e) => {
+	const touchMove = (e) => {
 
-   	if (dragStart === null) { return; }
+		if (dragStart === null) { return; }
 
-   	if (e.originalEvent.touches) {
-   		event = e.originalEvent.touches[0];
-   	}
+		if (e.originalEvent.touches) {
+			event = e.originalEvent.touches[0];
+		}
 
-   	delta = dragStart - event.clientY;
+		delta = dragStart - event.clientY;
 
-   	percentage = delta;
+		percentage = delta;
 
-      if (e.orgiginalEvent.detail < 0 || e.originalEvent.touches > 0) {
+		if (e.orgiginalEvent.detail < 0 || e.originalEvent.touches > 0) {
 
-         percentage--;
+			percentage--;
 
-         if (Math.abs(percentage) >= dragThreshold) {
-            prevSlide();
+			if (Math.abs(percentage) >= dragThreshold) {
+				prevSlide();
 				introChange();
-         }
-      } else  {
-         percentage++;
-         if(percentage >= dragThreshold) {
-            nextSlide();
+			}
+		} else  {
+			percentage++;
+			if(percentage >= dragThreshold) {
+				nextSlide();
 				introChange();
-         }
-   	}
+			}
+		}
+		// Don't drag element. This is important.
+		return false;
+	};
 
-   	// Don't drag element. This is important.
-   	return false;
-   };
+	const touchEnd = () => {
 
-   const touchEnd = () => {
+		dragStart = null;
 
-   	dragStart = null;
-
-   	if (percentage >= dragThreshold) {
-   		nextSlide();
+		if (percentage >= dragThreshold) {
+			nextSlide();
 			introChange();
-   	}
-   	else if ( Math.abs(percentage) >= dragThreshold ) {
-   		prevSlide();
+		}
+		else if ( Math.abs(percentage) >= dragThreshold ) {
+			prevSlide();
 			introChange();
-   	} else {
-   		// show current slide i.e. snap back
-   		showSlide();
-   	}
+		} else {
+			// show current slide i.e. snap back
+			showSlide();
+		}
 
-   	percentage = 0;
+		percentage = 0;
 
-   };
+	};
 
-   $('#projects').on({
-   	'touchstart': touchStart,
-   	'touchmove': touchMove,
-   	'touchend': touchEnd
-   });
-
-
-
+	//mobile event listener
+	$('#projects').on({
+		'touchstart': touchStart,
+		'touchmove': touchMove,
+		'touchend': touchEnd
+	});
 
 
-
-})(jQuery); //end window onload
+}); //end window onload
