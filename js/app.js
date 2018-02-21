0b691c9cfd7a28942da6fc99bde8370fc490b0d3
Geraldine Morales <geraldine.morales02@gmail.com>
2018-02-20 19:45:52 -0500

    added onload animations

--- js/app.js
+++ js/app.js
@@ -2,3 +2,21 @@
 $(document).ready(() => {
    $('.slideDown').slideDown(500, "swing");
 });
+
+
+jQuery(document).ready(function($) {
+  var alterClass = function() {
+    var ww = document.body.clientWidth;
+    if (ww < 768) {
+      $('.bio').addClass('vertical-sufi one');
+   } else if (ww >= 768) {
+      $('.bio').addClass('slide-up-fade-in three');
+      $('.bio').removeClass('vertical-sufi one');
+    }
+  };
+  $(window).resize(function(){
+    alterClass();
+  });
+  //Fire it when the page first loads:
+  alterClass();
+});
